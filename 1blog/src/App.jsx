c3283c442df5cdfe4fc38f2bfd2181b08efaf49f
Panda-rish <riteshkumar853892@gmail.com>
2024-04-15 14:32:34 +0530

    env

---
+++ 1blog/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_UR);
+   
+
+  return (
+    <>
+      <h1>blog app</h1>
+    </>
+  )
+}
+
+export default App
